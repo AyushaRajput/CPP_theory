a444452ee15ec0f95ac94cb5db9823ffe318c944
AyushaRajput <ayusha.rajput24@sanjivani.edu.in>
2024-08-31 23:19:42 +0530

    Add files via upload

---
+++ operators.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+int a=5, b=6;
+
+//addition operatoe
+cout<< "a+b="<<(a+b)<<endl;
+
+//substraction operator
+cout<<"a-b="<<(a-b)<<endl;
+
+//multiplication operator
+cout << "a*b="<<(a*b)<<endl;
+
+//division operator
+cout << "a/b="<<(a/b)<< endl;
+
+return 0;
+}
