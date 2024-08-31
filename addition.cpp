56192b4e0390aafeabb0bf5c7174b910216bc76a
AyushaRajput <ayusha.rajput24@sanjivani.edu.in>
2024-08-31 23:20:45 +0530

    Add files via upload

---
+++ addition.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+int first_number, second_number, sum ;
+
+cout<<"enter a integer";
+cin>> first_number>>second_number;
+
+//sum of two numbers is stored in variable sumoftwonumber
+
+sum = first_number + second_number;
+
+//printsum
+cout<<first_number<<"+"<<second_number<<"="<<sum;
+
+return 0;
+}
