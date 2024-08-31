98ddb0a9718f7d6c3c13fa57d056ec429000cb95
AyushaRajput <ayusha.rajput24@sanjivani.edu.in>
2024-08-31 23:30:50 +0530

    Add files via upload

---
+++ even or odd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+int a;
+cout<<"please enter an integer"<<endl;
+cin>> a;
+
+if (a%2==0){;
+cout<<"the given number is even";
+}
+else{
+  cout<<"the given number is odd";
+}
+return 0;
+}
