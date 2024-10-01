e2130d9a11b57f10b57007ea6a405b4c0e508092
AyushaRajput <ayusha.rajput24@sanjivani.edu.in>
2024-10-01 20:28:30 +0530

    Add files via upload

---
+++ Mathematical functions.cpp
@@ -0,0 +1,10 @@
+#include <cmath>
+#include <iostream> 
+using namespace std;
+
+int main() 
+{
+  double num = 25.0;
+  cout<<"Square root if  25 is:"<<sqrt(num)<<endl; cout << endl;
+    return 0;
+}
